36616dccae183fa91c2e5c8b2cb6b048f959e0a1
Walter Marinho <waltersmarinho@edu.unifil.br>
2022-04-26 21:23:36 -0300

    Seleção de alunos para geração das declarações

--- resources/views/coordenador/declaracoes.blade.php
+++ resources/views/coordenador/declaracoes.blade.php
@@ -1,33 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!--<link href="{{ asset('css/app.css') }}" rel="stylesheet">-->
     <title>Document</title>
+    <style>
+        td {
+            text-align: center;
+        }
+    </style>
 </head>
+
 <body class="antialiased">
-    <h1>Gerar declarações</h1>
-    <p>{{ $alunos }}</p>-->
-    <div class="flex">
-        <p>Nome</p>
-        <p>Turma</p>
-        <p>Curso</p>
-        <p>Atividades</p>
-        <p>Horas</p>
-        <p>Orientador</p>
-    </div>
-        @foreach ($alunos as $aluno) 
-        <div class="flex">
-            <p>{{ $aluno['nome_aluno'] }}</p>
-            <p>{{ $aluno['turma_id'] }}</p>
-            <p>{{ $aluno['curso'] }}</p>
-            <p>ok</p>
-            <p>0/80</p>
-            <p>{{ $aluno['orientador_id'] }}</p>
-        </div>
-        @endforeach
+    <h1 style="font-size: 1.85em; font-weight:bold">Gerar declarações</h1>
+    <!--<p>{{ $alunos }}</p>-->
+    <form action="" method="get" id="selecao_alunos">
+        <button onclick="enviar_dados()">Gerar declarações</button>
+        <table width=100% center>
+            <tr>
+                <th></th>
+            <th>Nome</th>
+            <th>Turma</th>
+            <th>Curso</th>
+            <th>Atividades</th>
+            <th>Horas</th>
+            <th>Orientador</th>
+            </th>
+            @foreach ($alunos as $aluno)
+            <tr>
+                <td>
+                    <input type="checkbox" name="{{ $aluno['id'] }}" id="{{ $aluno['id'] }}" value="{{ $aluno }}">
+                </td>    
+                <td center>{{ $aluno['nome_aluno'] }}</td>
+                <td center>{{ $aluno['turma_id'] }}</td>
+                <td center>{{ $aluno['curso'] }}</td>
+                <td center>ok</td>
+                <td center>0/80</td>
+                <td center>{{ $aluno['orientador_id'] }}</td>
+            </tr>
+            @endforeach
+        </table>
         
+    </form>
+
+    <script>
+        function enviar_dados() {
+            console.log(document.getElementById("selecao_alunos"))
+        }
+    </script>
 </body>
+
 </html>
