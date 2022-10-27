c05fbf4677e830fbe1fa68a2c5e9364459cce406
Walter Schmidt Marinho <waltersmarinho@edu.unifil.br>
2022-10-27 19:19:33 -0300

    Página de documentos no coordenador

--- resources/views/layouts/sideNavigation.blade.php
+++ resources/views/layouts/sideNavigation.blade.php
@@ -111,6 +111,23 @@
 
                 <hr class="hidden group-hover:block border-y-[1px] border-y-gray">
 
+                <li>
+                    <x-nav-link class="hidden group-hover:flex" :href="route('coordenador_docpage')" :active="request()->routeIs('coordenador_docpage')">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-files mr-2"
+                            width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                            stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M15 3v4a1 1 0 0 0 1 1h4"></path>
+                            <path d="M18 17h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h4l5 5v7a2 2 0 0 1 -2 2z"></path>
+                            <path d="M16 17v2a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h2"></path>
+                        </svg>
+                        {{ __('Documentos') }}
+
+                    </x-nav-link>
+                </li>
+
+                <hr class="hidden group-hover:block border-y-[1px] border-y-gray">
+
                 <br>
             @endif
 
