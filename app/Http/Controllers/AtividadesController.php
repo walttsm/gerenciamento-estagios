2a7dcabb606d868c2c4b8dbdb177cd6dd75d1b0b
Walter Schmidt Marinho <waltersmarinho@edu.unifil.br>
2022-10-27 10:42:21 -0300

    Correções para buscar o aluno corretamente

--- app/Http/Controllers/AtividadesController.php
+++ app/Http/Controllers/AtividadesController.php
@@ -11,60 +11,68 @@ use Illuminate\Http\Request;
 
 class AtividadesController extends Controller
 {
-    public function listarAtividadesAluno(){
+    public function listarAtividadesAluno()
+    {
         $atividades = Atividade::all();
 
-        $id_user = Auth::user()->id;
-        $aluno = Aluno::where('user_id', $id_user)->first();
+        $user = Auth::user();
+        $aluno = Aluno::where('email', $user->email)->first();
         $a = AtividadeFile::where('aluno_id', $aluno->id)
-                ->get();
-        return view('aluno.atividades', ['atv'=> $atividades, 'envio' => $a]);
+            ->get();
+        return view('aluno.atividades', ['atv' => $atividades, 'envio' => $a]);
     }
 
-    public function listarAtividades(){
+    public function listarAtividades()
+    {
         $atividades = Atividade::all();
-        return view('coordenador.atividades', ['atv'=> $atividades]);
-    }
-    
-    public function infoAtividade($id){
-        $atividades = Atividade::find($id);
-        
-        return view('coordenador.atividadesinfo', ['atv'=> $atividades]);
+        return view('coordenador.atividades', ['atv' => $atividades]);
     }
 
-    public function deleteAtividade($id){
+    public function infoAtividade($id)
+    {
+        $atividades = Atividade::find($id);
+
+        return view('coordenador.atividadesinfo', ['atv' => $atividades]);
+    }
+
+    public function deleteAtividade($id)
+    {
         $atv = Atividade::find($id);
         $atv->delete();
 
         return redirect('coordenador/atividades');
     }
 
-    public function criarAtividades(Request $request){
+    public function criarAtividades(Request $request)
+    {
         $atv = new Atividade();
         $atv->nome_atividade = $request->nome_atividade;
-        if($request->doc_nome != null){
+        if ($request->doc_nome != null) {
             $atv->descricao = $request->doc_nome;
         }
-        
+
         $atv->save();
         return redirect('coordenador/atividades');
     }
 
-    public function create(){
+    public function create()
+    {
         return view('coordenador.criaratv');
     }
 
-    public function edit($id){
+    public function edit($id)
+    {
         $atv = Atividade::find($id);
-        return view('coordenador.editatv',['atv' => $atv]);
+        return view('coordenador.editatv', ['atv' => $atv]);
     }
-    public function editAtividade(Request $request, $id){
+    public function editAtividade(Request $request, $id)
+    {
         $atv = Atividade::find($id);
 
         $nome_atividade = $request->nome_atividade;
-        if($request->doc_nome != null){
+        if ($request->doc_nome != null) {
             $descricao = $request->descricao;
-        }else{
+        } else {
             $descricao = null;
         }
         $data = $request->only('descricao', 'nome_atividade');
@@ -72,34 +80,35 @@ class AtividadesController extends Controller
         return redirect('coordenador/atividades');
     }
 
-    public function infoAtividadeAluno($id){
+    public function infoAtividadeAluno($id)
+    {
         $atividades = Atividade::find($id);
         $id_user = Auth::user()->id;
         $aluno = Aluno::where('user_id', $id_user)->first();
 
         $data = AtividadeFile::where('aluno_id', $aluno->id)
-                ->where('atividade_id', $id)
-                ->orderby('created_at', 'desc')
-                ->first();
+            ->where('atividade_id', $id)
+            ->orderby('created_at', 'desc')
+            ->first();
 
         $files = AtividadeFile::where('aluno_id', $aluno->id)
-                ->where('atividade_id', $id)
-                ->get();
+            ->where('atividade_id', $id)
+            ->get();
 
-        if($data == null){
-            return view('aluno.atividadesinfo', ['atv'=> $atividades, 'entrega' => $data]);
-        }else{
-            return view('aluno.atividadesinfo', ['atv'=> $atividades, 'entrega' => $data, 'files' => $files]);
+        if ($data == null) {
+            return view('aluno.atividadesinfo', ['atv' => $atividades, 'entrega' => $data]);
+        } else {
+            return view('aluno.atividadesinfo', ['atv' => $atividades, 'entrega' => $data, 'files' => $files]);
         }
-        
     }
 
-    public function enviarAtividade(Request $request, $id){
-        $id_user = Auth::user()->id;
-        $aluno = Aluno::where('user_id', $id_user)->first();
+    public function enviarAtividade(Request $request, $id)
+    {
+        $id_user = Auth::user();
+        $aluno = Aluno::where('email', $id_user->email)->first();
         $files = $request->filenames;
-        if($request->hasFile('filenames')){
-            foreach($request->filenames as $file){
+        if ($request->hasFile('filenames')) {
+            foreach ($request->filenames as $file) {
                 $atv = new AtividadeFile;
                 $atv->arquivo_title = $file->getClientOriginalName();
                 $atv->local_arquivo = $file->store('AtividadesFiles');
@@ -108,27 +117,28 @@ class AtividadesController extends Controller
                 $atv->save();
             }
         }
-        
+
         return back();
     }
 
-    public function editarEnvioAtividade(Request $request, $id){
-        $id_user = Auth::user()->id;
-        $aluno = Aluno::where('user_id', $id_user)->first();
+    public function editarEnvioAtividade(Request $request, $id)
+    {
+        $id_user = Auth::user();
+        $aluno = Aluno::where('email', $id_user->email)->first();
         $atv = AtividadeFile::where('aluno_id', $aluno->id)
-                ->where('atividade_id', $id)
-                ->get();
+            ->where('atividade_id', $id)
+            ->get();
 
         $filesAnt = $request->filenamesAnt;
-        foreach($atv as $file){
-            if (!in_array($file->id, $filesAnt)){
+        foreach ($atv as $file) {
+            if (!in_array($file->id, $filesAnt)) {
                 $file->delete();
             }
-        }    
+        }
 
         $files = $request->filenames;
-        if($request->hasFile('filenames')){
-            foreach($request->filenames as $file){
+        if ($request->hasFile('filenames')) {
+            foreach ($request->filenames as $file) {
                 $atv = new AtividadeFile;
                 $atv->arquivo_title = $file->getClientOriginalName();
                 $atv->local_arquivo = $file->store('AtividadesFiles');
@@ -139,6 +149,5 @@ class AtividadesController extends Controller
         }
 
         return back();
-        
     }
 }
